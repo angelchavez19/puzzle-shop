0a4ff46475e987644f5834df8fab3d7f9a2bd5c0
angelchavez19 <infoangelchavez@gmail.com>
2025-06-06 17:50:22 -0500

    chore(backend/category): change CategoryNotFound to CategoryNotFoundException

--- backend/src/contexts/category/domain/exceptions/category-not-found.exception.ts
+++ backend/src/contexts/category/domain/exceptions/category-not-found.exception.ts
@@ -1,6 +1,6 @@
 import { HttpException } from 'src/shared';
 
-export class CategoryNotFound extends HttpException {
+export class CategoryNotFoundException extends HttpException {
   constructor(id: string) {
     super(`Category with ID ${id} not found`, 404);
   }
